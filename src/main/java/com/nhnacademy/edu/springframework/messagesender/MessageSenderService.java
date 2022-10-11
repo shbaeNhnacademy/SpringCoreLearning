05744d7c6c33b3400ad5765b7baa8b041a9ee52c
배수한 <qotngks152@naver.com>
2022-10-11 16:11:19 +0900

    DI 실습(setter)

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSenderService.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/MessageSenderService.java
@@ -1,13 +1,22 @@
 package com.nhnacademy.edu.springframework.messagesender;
 
 public class MessageSenderService {
-    private final MessageSender messageSender;
+    private MessageSender messageSender;
+//
+//    public MessageSenderService(MessageSender messageSender) {
+//        this.messageSender = messageSender;
+//    }
 
-    public MessageSenderService(MessageSender messageSender) {
+
+    public MessageSenderService() {
+    }
+
+    public void setMessageSender(MessageSender messageSender) {
+        System.out.println("MessageSenderService.setMessageSender");
         this.messageSender = messageSender;
     }
 
-    public void doSendMessage(User user,String message) {
+    public void doSendMessage(User user, String message) {
         messageSender.sendMessage(user, message);
     }
 }
