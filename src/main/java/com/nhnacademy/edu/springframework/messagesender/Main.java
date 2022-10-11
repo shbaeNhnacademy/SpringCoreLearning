f40c02727fdf15a109bedc727a58c6511db3c02d
배수한 <qotngks152@naver.com>
2022-10-11 11:07:08 +0900

    다형성 실습 2까지

--- src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/messagesender/Main.java
@@ -2,16 +2,10 @@ package com.nhnacademy.edu.springframework.messagesender;
 
 public class Main {
     public static void main(String[] args) {
-        Main main = new Main();
         User user = new User("a@b.com", "8201099914501");
-        main.sendSmsMessage(user, "hello Message");
+       new MessageSendService(new SmsMessageSender()).doSendMessage(user, "hello message");
+       new MessageSendService(new EmailMessageSender()).doSendMessage(user, "hello message");
     }
 
-    private void sendSmsMessage(User user, String message) {
-        System.out.println("SMS Message Sent to " + user.getPhoneNumber() + " : " + message);
-    }
 
-    private void sendEmailMessage(User user, String message) {
-        System.out.println("EMAIL Message Sent to " + user.getEmail() + " : " + message);
-    }
 }
